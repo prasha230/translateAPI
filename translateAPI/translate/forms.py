c2d108751b1f2d857c9b236d1fbc8143df1f4615
Prashant Rai <prashantrai230@gmail.com>
2022-06-22 19:41:15 +0530

    Text to Speech added

--- translateAPI/translate/forms.py
+++ translateAPI/translate/forms.py
@@ -5,6 +5,5 @@ from .models import LanguageModel
 
 class TranslateForm(forms.Form):
     inp_text=forms.CharField(label='', max_length=100,widget=forms.Textarea(attrs={'placeholder': 'Input','class':'form-control','rows':5,'columns':15}))
-    # lang=forms.CharField(label='', max_length=100,widget=forms.TextInput(attrs={'placeholder': 'Language Code'}))
-    l=forms.ModelChoiceField(label='',queryset=LanguageModel.objects.all(),empty_label="Select Language",widget=forms.Select(attrs={'class':'form-select'}))
+    lang=forms.ModelChoiceField(label='',queryset=LanguageModel.objects.all(),empty_label="Select Language",widget=forms.Select(attrs={'class':'form-select'}))
     
--- translateAPI/translate/views.py
+++ translateAPI/translate/views.py
@@ -1,23 +1,23 @@
-from django.shortcuts import render
-
+from django.shortcuts import redirect, render
 from .models import LanguageModel
 from .forms import TranslateForm
-from django.http import HttpResponseRedirect
 import requests
+from gtts import gTTS
 
 # Create your views here.
 def home(request):
-    submitted=False
+    submitted = False
     if request.method == 'POST':
         form = TranslateForm(request.POST)
         if form.is_valid():
-            inp=form.cleaned_data['inp_text']
-            # lang=form.cleaned_data['lang']
-            sl=form.cleaned_data['l']
-            lc=LanguageModel.objects.all().filter(Language=sl).values('Code')[0]['Code']
-            print(lc)
+            inp = form.cleaned_data['inp_text']
+            sl = form.cleaned_data['lang']
+            lang_code = LanguageModel.objects.all().filter(Language=sl).values('Code')[0]['Code']
+
+            # api
             url = "https://microsoft-translator-text.p.rapidapi.com/translate"
-            querystring = {"to":lc,"api-version":"3.0","profanityAction":"NoAction","textType":"plain"}
+            querystring = {"to": lang_code, "api-version": "3.0",
+                           "profanityAction": "NoAction", "textType": "plain"}
             payload = [{"Text": inp}]
             headers = {
                 "content-type": "application/json",
@@ -25,12 +25,16 @@ def home(request):
                 "X-RapidAPI-Key": "0a13a46b22mshff6228808313caep1db3e6jsndf0028c96919"
             }
             response = requests.request("POST", url, json=payload, headers=headers, params=querystring).json()
-            form=TranslateForm
-            return render(request,'translate/result.html',{'inp':inp,'response':response[0]['translations'][0]['text']})
+            
+            #making audio files
+            myobj = gTTS(text=inp, lang='en', slow=False)  
+            myobj.save('translate/static/inp.mp3')
+            myobj = gTTS(text=response[0]['translations'][0]['text'], lang=lang_code, slow=False)  
+            myobj.save('translate/static/response.mp3')
+
+            return render(request, 'translate/result.html', {'inp': inp, 'response': response[0]['translations'][0]['text'],'sl':sl})
     else:
         form = TranslateForm
         if 'submitted' in request.GET:
-            submitted=True
-    return render(request,'translate/home.html',{'form':form,'submitted':submitted})
-    
-
+            submitted = True
+    return render(request, 'translate/home.html', {'form': form, 'submitted': submitted})
